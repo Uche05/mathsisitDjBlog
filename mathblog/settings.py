28bdc17f77fcaa0b877a5d6fc123839efbbbdfd7
Uche05 <uchekpadeuwa@gmail.com>
2025-12-16 08:43:57 +0000

    add CSRF trusted origins for dashboard access

--- mathblog/settings.py
+++ mathblog/settings.py
@@ -147,3 +147,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # for collectstatic in deployment
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://mathisitnotes-dae49d933559.herokuapp.com/dashboard/',
+    'http://127.0.0.1',
+    'http://localhost:8000',
+]
